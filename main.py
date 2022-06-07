7bbba461cfb81a8876e87fe235aa103dbd045ec3
StrangeTheory <rflgnv@gmail.com>
2022-06-07 19:24:07 +0300

    GOOD verison of code structure

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from config import OW_token
+from pprint import pprint
 import requests
+import datetime
 
 def get_weather(city,OW_token):
     try:
         r = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OW_token}"
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OW_token}&units=metric"
         )
         data = r.json()
-        print(data)
+        #pprint(data)
+
+        city = data["name"]
+        cur_temp = data["main"]["temp"]
+        humid = data["main"]["humidity"]
+        wind = data["wind"]["speed"]
+        sunrise_time = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_time = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+
+        print(f"Погода в городе: {city}\nТемпература: {cur_temp}C\n"
+        f"Влажность: {humid}%\nВетер: {wind}м.c\n"
+        f"Время восхода: {sunrise_time}\n"
+        f"Время заката: {sunset_time}" )
+
 
     except Exception as ex:
         print(ex)
