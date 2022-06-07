11b17427cb77bc2c75477ec8d3cc2a9b58bbebd4
StrangeTheory <rflgnv@gmail.com>
2022-06-07 19:54:02 +0300

    GOOD verison of code structure

--- main.py
+++ main.py
@@ -4,12 +4,30 @@ import requests
 import datetime
 
 def get_weather(city,OW_token):
+
+    code_to_smile = {
+        "Clear": "Ясно \U00002688",
+        "Clouds": "Облачно \U00002681",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туманище \U0001F328"
+    }
+
+
     try:
         r = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OW_token}&units=metric"
         )
         data = r.json()
         #pprint(data)
+
+        weather_description = data["weather"]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно"
 
         city = data["name"]
         cur_temp = data["main"]["temp"]
@@ -18,7 +36,7 @@ def get_weather(city,OW_token):
         sunrise_time = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
         sunset_time = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
 
-        print(f"Погода в городе: {city}\nТемпература: {cur_temp}C\n"
+        print(f"Погода в городе: {city}\nТемпература: {cur_temp}C {wd}\n"
         f"Влажность: {humid}%\nВетер: {wind}м.c\n"
         f"Время восхода: {sunrise_time}\n"
         f"Время заката: {sunset_time}" )
