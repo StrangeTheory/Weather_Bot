8c7ed0d4b12246415b38cbedd27f0fef30763111
StrangeTheory <rflgnv@gmail.com>
2022-06-09 01:32:26 +0300

    code syntax fixed

--- main_tg.py
+++ main_tg.py
@@ -1,15 +1,15 @@
 import requests
 import datetime
 from config import tg_token, OW_token
-from aiogram import bot, types
-from aiogram.dispatcher import dispatcher
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
 bot = Bot(token=tg_token)
-dp = dispatcher(bot)
+dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
-async_def start_command(message: types.message):
+async def start_command(message: types.message):
     await message.reply("Привет! Напиши мне название города и я подскажу тебе прогноз!")
 
 
